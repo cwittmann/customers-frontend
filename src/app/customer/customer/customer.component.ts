8b253666e1667f7389836757a8a8857ea8471b33
cwittmann <christofwittmann@web.de>
2020-04-14 22:26:01 +0200

    fix: crud api methods with type

--- src/app/customer/customer/customer.component.ts
+++ src/app/customer/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Customer } from 'src/app/shared/interfaces/customer'
+import { Customer } from 'src/app/shared/model/customer'
 import { CustomerService } from 'src/app/shared/services/customer/customer.service'
 import { Observable } from 'rxjs/Observable'
 
@@ -10,11 +10,11 @@ import { Observable } from 'rxjs/Observable'
 })
 export class CustomerComponent implements OnInit {
 
-  customers: Observable<Customer[]>;
+  customers: Customer[];
 
   constructor(private customerService: CustomerService) { }
 
-  ngOnInit(): void {
-    this.customers = this.customerService.getAllCustomers();
+  ngOnInit() {
+    this.customerService.getAllCustomers().subscribe((data: Customer[]) => {this.customers = data; });
   }
 }
--- src/app/shared/services/customer/customer.service.ts
+++ src/app/shared/services/customer/customer.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
-import { Customer} from 'src/app/shared/interfaces/customer'
+import { Customer} from 'src/app/shared/model/customer'
 import { HttpClient } from '@angular/common/http'
 
 @Injectable({
@@ -10,28 +10,27 @@ export class CustomerService {
 
   constructor(private http: HttpClient) {}
 
-  getAllCustomers(): Observable<Customer[]> {
-    var customers = this.http.get<Customer[]>('http://localhost:8000/api/customers');
+  getAllCustomers() {
+    var customers = this.http.get('http://localhost:8000/api/customers');
     return customers;
   }
 
-  getCustomer(name: string): Observable<Customer> {
-    return this.http.get<Customer>('http://localhost:8000/api/customers/' + name)
+  getCustomer(name: string) {
+    return this.http.get('http://localhost:8000/api/customers/' + name);
   }
 
-  insertCustomer(Customer: Customer): Observable<Customer> {
-    return this.http.post<Customer>('http://localhost:8000/api/customers/', Customer)
+  insertCustomer(Customer: Customer) {
+    return this.http.post('http://localhost:8000/api/customers/', Customer);
   }
 
-  updateCustomer(Customer: Customer): Observable<void> {
-    return this.http.put<void>(
+  updateCustomer(Customer: Customer) {
+    return this.http.put(
       'http://localhost:8000/api/customers/' + Customer.name,
       Customer
-    )
+    );
   }
 
   deleteCustomer(name: string) {
     return this.http.delete('http://localhost:8000/api/customers/' + name)
   }
-
 }
