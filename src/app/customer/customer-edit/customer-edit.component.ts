4e3ca826cf6c2b46d036a7aee76b883835128a82
cwittmann <christofwittmann@web.de>
2020-04-17 22:30:42 +0200

    fix: validation

--- src/app/customer/customer-edit/customer-edit.component.ts
+++ src/app/customer/customer-edit/customer-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Customer } from 'src/app/shared/model/customer';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from 'src/app/shared/services/customer/customer.service';
-import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-edit',
@@ -14,14 +14,23 @@ export class CustomerEditComponent implements OnInit {
   id: any;
   isNew: boolean;
   customer: Customer;
-  editForm: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute, private customerService: CustomerService) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private customerService: CustomerService) {}
+ 
+  saveData(event){
+      const allCustomerValuesValid = !Object.values(this.customer).some(x => (x == null || x == ''));
+      
+      if(allCustomerValuesValid){
+        this.customerService.updateCustomer(this.customer).subscribe(() => {
+            this.router.navigate([""]);
+          });          
+      }      
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
     this.id = this.activatedRoute.snapshot.params.id;
     this.customerService.getCustomer(this.id).subscribe((data: Customer[]) => {
-      this.customer = data[0];          
+      this.customer = data[0]; 
     });    
   }
 }
