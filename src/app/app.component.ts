3dba1900105a8ba973b4edac481bbb642492bd02
cwittmann <christofwittmann@web.de>
2020-04-29 18:23:18 +0200

    feat: connection service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { IndexedDatabaseService } from './shared/services/database/indexed-database.service';
 import { Router } from '@angular/router';
 import { AuthService } from './shared/services/authentication/auth.service';
+import { ConnectionService } from './shared/services/connection/connection.service';
 
 @Component({
   selector: 'app-root',
@@ -17,17 +18,21 @@ export class AppComponent {
   title = 'customers';
   indexedDatabaseService: IndexedDatabaseService;
   isAuthenticated: Boolean = false;
+  isOnline: Boolean;
 
   constructor(
     public dialog: MatDialog,
     private IndexedDatabaseService: IndexedDatabaseService,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private connectionService: ConnectionService
   ) {
     authService.userAuthenticated.subscribe((isAuthenticated) => {
-      console.log(isAuthenticated);
       this.isAuthenticated = isAuthenticated;
-      console.log(this.isAuthenticated);
+    });
+
+    connectionService.connectionChanged.subscribe((isOnline) => {
+      this.isOnline = isOnline;
     });
   }
 
---
+++ src/app/shared/services/connection/connection.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConnectionService {
+  interval: any;
+  connectionChanged: EventEmitter<Boolean>;
+
+  constructor(private http: HttpClient) {
+    this.connectionChanged = new EventEmitter();
+    this.InitializeConnectionCheck();
+  }
+
+  InitializeConnectionCheck() {
+    this.interval = setInterval(async () => {
+      await this.http
+        .get('http://localhost:8000/api/connect')
+        .toPromise()
+        .then(() => {
+          this.connectionChanged.emit(true);
+        })
+        .catch((error) => {
+          this.connectionChanged.emit(false);
+        });
+    }, 5000);
+  }
+}
